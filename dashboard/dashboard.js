f16bf68c72f712dae2a41808646ada98b943f2b6
Pierre Mpolet <pierempolet59@gmail.com>
2024-08-18 03:12:44 +0100

    j'ai ajoue la partie userDashboard,

--- dashboard/dashboard.js
+++ dashboard/dashboard.js
@@ -1,14 +1,14 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const navLinks = document.querySelectorAll('.nav-link');
-
+document.addEventListener('DOMContentLoaded', function () {
+    // Get the current page URL
+    const currentPage = window.location.pathname.split('/').pop();
+    
+    // Get all navigation links
+    const navLinks = document.querySelectorAll('.sidebar nav ul li a');
+    
     navLinks.forEach(link => {
-        if (link.classList.contains('active')) {
+        // Check if the href attribute matches the current page URL
+        if (link.getAttribute('href') === currentPage) {
             link.classList.add('active');
         }
-
-        link.addEventListener('click', () => {
-            navLinks.forEach(link => link.classList.remove('active'));
-            link.classList.add('active');
-        });
     });
 });
